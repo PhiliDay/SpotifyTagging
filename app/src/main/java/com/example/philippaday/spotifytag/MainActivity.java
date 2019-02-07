90a115c039ea535fbc955e9ffb345467d731bebb
Phili Day <philippa.day@tigerspike.com>
2019-02-07 11:20:35 +0000

    Remove unneccessary shiz

--- app/src/main/java/com/example/philippaday/spotifytag/MainActivity.java
+++ app/src/main/java/com/example/philippaday/spotifytag/MainActivity.java
@@ -1,31 +1,17 @@
 package com.example.philippaday.spotifytag;
 
-import android.content.ContentValues;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.provider.BaseColumns;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 
-import com.spotify.protocol.client.CallResult;
-import com.spotify.protocol.client.Subscription;
-import com.spotify.protocol.types.PlayerState;
 import com.spotify.protocol.types.Track;
 
 
